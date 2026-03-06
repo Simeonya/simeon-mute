3da23dc300252675ca0daa00ef2d0a50b9743af6
Simeonya <kruschsimeon@gmail.com>
2026-03-06 18:37:21 +0100

    Fix ui_page and shared_script paths in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,8 @@ author({ 'Simeonya' })
 description({ 'A simple script that allows you to mute yourself in the voice chat by pressing a key.' })
 version({ '1.0.1' })
 
-<<<<<<< HEAD
 ui_page({ 'html/index.html' });
 shared_script({ 'shared/*' });
-=======
-ui_page('html/index.html');
-shared_script('shared/config.lua');
->>>>>>> 80db9d7a6c086ca06a39bbf3177bf437578a72cf
 
 server_scripts({
     'server/main.lua'
